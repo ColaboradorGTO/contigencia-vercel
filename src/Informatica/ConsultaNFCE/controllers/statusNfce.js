7a0ede55fa8b20e6716b2f1c168b5fffc665742e
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-13 10:29:41 -0300

    refactor: node modules

--- src/Informatica/ConsultaNFCE/controllers/statusNfce.js
+++ src/Informatica/ConsultaNFCE/controllers/statusNfce.js
@@ -182,16 +182,16 @@ class ConsultaStatusNfeController {
       const tools = new Tools(toolsConfig, certOptions);
       
       console.log('⏳ Consultando status SEFAZ para chave:', chave);
-      // const resposta = await tools.sefazStatus(chave).catch(err => {
-      //   console.error('❌ Erro ao consultar status da SEFAZ:', err.message);
-      //   throw err;
-      // });
+      const resposta = await tools.sefazStatus(chave).catch(err => {
+        console.error('❌ Erro ao consultar status da SEFAZ:', err.message);
+        throw err;
+      });
       
 
  
       return res.json({
         vendaData,
-        // xml: resposta
+        xml: resposta
       });
     } catch (error) {
       console.error('❌ Erro completo:', error);
