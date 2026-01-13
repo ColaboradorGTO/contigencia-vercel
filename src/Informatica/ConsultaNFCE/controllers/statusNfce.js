441738c01907e505febb3190866ec4236df3952d
Myltiane Alves <myltiane.aux@gmail.com>
2026-01-13 11:11:56 -0300

    refactor:

--- src/Informatica/ConsultaNFCE/controllers/statusNfce.js
+++ src/Informatica/ConsultaNFCE/controllers/statusNfce.js
@@ -135,11 +135,11 @@ class ConsultaStatusNfeController {
       process.env.OPENSSL_MODULES = opensslModulesPath;
       
       // Detectar SO para usar os caminhos corretos
-      const isWindows = process.platform === 'win32';
-      const xmllintPath = isWindows ? path.resolve("./libs/libxml/bin/xmllint.exe") : "xmllint";
-      const opensslPath = isWindows ? path.resolve("./libs/openssl/bin/openssl.exe") : "openssl";
+      // const isWindows = process.platform === 'win32';
+      // const xmllintPath = isWindows ? path.resolve("./libs/libxml/bin/xmllint.exe") : "xmllint";
+      // const opensslPath = isWindows ? path.resolve("./libs/openssl/bin/openssl.exe") : "openssl";
       
-      
+
       const toolsConfig = {
         mod: mod,
         tpAmb: tpAmb,
@@ -148,8 +148,8 @@ class ConsultaStatusNfeController {
         timeout: 180000, // 
         CSC: csc,
         CSCid: cscId,
-        xmllint: xmllintPath,
-        openssl: opensslPath,
+        // xmllint: xmllintPath,
+        // openssl: opensslPath,
       };
       
       console.log('✅ Dados da venda carregados com sucesso');
